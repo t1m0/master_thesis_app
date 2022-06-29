dc18a22a17222878049164023c8ef5f397f1f551
Timo Schöpflin <timo@schoepflin.io>
2022-06-29 09:14:56 +0200

    Add uuid to result

--- src/components/spiral/model/SpiralDrawingResult.ts
+++ src/components/spiral/model/SpiralDrawingResult.ts
@@ -1,10 +1,13 @@
+import { v4 as uuid } from 'uuid';
 import  ImageCoordinate  from "./ImageCoordinate";
 import ImageWrapper from "./ImageWrapper";
 export default class SpiralDrawingResult {
+    uuid:string;
     imageWrapper:ImageWrapper;
     start:ImageCoordinate;
     end:ImageCoordinate;
     constructor(imageWrapper:ImageWrapper, start:ImageCoordinate, end:ImageCoordinate) {
+        this.uuid = uuid();
         this.imageWrapper = imageWrapper;
         this.start = start;
         this.end = end;
