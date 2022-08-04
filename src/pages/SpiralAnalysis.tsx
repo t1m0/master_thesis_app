b94770cc9f6b243c7f874249a160c3039011c406
Timo Schöpflin <timo@schoepflin.io>
2022-08-04 12:25:52 +0200

    Update result table display

--- src/pages/SpiralAnalysis.tsx
+++ src/pages/SpiralAnalysis.tsx
@@ -1,6 +1,6 @@
 import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import SpiralRating from '../components/spiral/algorithm/SpiralRating';
 import SpiralDrawing from '../components/spiral/model/SpiralDrawing';
 import SpiralDrawingRating from '../components/spiral/model/SpiralDrawingRating';
@@ -13,10 +13,10 @@ const SpiralAnalysis: React.FC = () => {
   const [result, setResult] = useState<SpiralDrawingRating | undefined>(undefined);
   const params = useParams();
   const uuid = params["uuid"] as string;
-  
+
   useEffect(() => {
     readFromStorage<SpiralDrawing>(uuid).then(d => {
-      if(d != undefined) {
+      if (d != undefined) {
         console.log(d);
         setDrawing(d);
         const result = spiralRating.rate(d);
@@ -27,21 +27,36 @@ const SpiralAnalysis: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if(drawing != undefined) {
+    if (drawing != undefined) {
       const result = spiralRating.rate(drawing);
       console.log(result);
       setResult(result);
     }
   }, [drawing]);
 
-  
+  const getTableBody = () => {
+    if (result != undefined && drawing != undefined) {
+      return <tbody>
+        <tr><td>1th Order Smoothness</td><td>{result.firstOrderSmoothness}</td></tr>
+        <tr><td>2nd Order Smoothness</td><td>{result.secondOrderSmoothness}</td></tr>
+        <tr><td>Tightness</td><td>{result.thightness}</td></tr>
+        <tr><td>Zero Crossing Rate</td><td>{result.zeroCrossingRate}</td></tr>
+        <tr><td>Degree of Severity</td><td>{result.degreeOfSeverity}</td></tr>
+        <tr><td>Severity Level</td><td>{result.severityLevel}</td></tr>
+        <tr><td>Total Time</td><td>{Math.round(drawing.time / 1000 * 100) / 100}sec</td></tr>
+      </tbody>
+    } else {
+      return <tbody></tbody>
+    }
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Spiral Analysis {uuid}</IonTitle>
           <IonButtons>
-            <IonBackButton defaultHref='/spiral'/>
+            <IonBackButton defaultHref='/spiral' />
           </IonButtons>
         </IonToolbar>
       </IonHeader>
@@ -53,17 +68,9 @@ const SpiralAnalysis: React.FC = () => {
               <th>Value</th>
             </tr>
           </thead>
-          <tbody>
-            <tr><td>1th Order Smoothness</td><td>{result?.firstOrderSmoothness}</td></tr>
-            <tr><td>2nd Order Smoothness</td><td>{result?.secondOrderSmoothness}</td></tr>
-            <tr><td>Tightness</td><td>{result?.thightness}</td></tr>
-            <tr><td>Zero Crossing Rate</td><td>{result?.zeroCrossingRate}</td></tr>
-            <tr><td>Degree of Severity</td><td>{result?.degreeOfSeverity}</td></tr>
-            <tr><td>Severity Level</td><td>{result?.severityLevel}</td></tr>
-            <tr><td>Total Time</td><td>{drawing?.time}</td></tr>
-          </tbody>
+          {getTableBody()}
         </table>
-        
+
         {drawing?.accelerations.map(v => <p>count: {v.recordCount} xAxis: {v.xAxis} yAxis: {v.yAxis} zAxis: {v.zAxis}</p>)}
       </IonContent>
     </IonPage>
